d316536bfe9b80a7bbd5d241d0b7feb3bef9f230
gitLouis <plusel@gmail.com>
2016-09-22 08:55:34 +0300

    test commit

--- Untitled.py
+++ Untitled.py
@@ -2,66 +2,63 @@
 # coding: utf-8
 
 import pandas
-
-meetings_train_raw = pandas.read_csv("C:\\Users\\Maria\\Dropbox\\Sea Snails\\raw data\\meetings_train.csv")
-port_visits_train_raw = pandas.read_csv("C:\\Users\\Maria\\Dropbox\\Sea Snails\\raw data\\port_visits_train.csv")
-vessels_labels_train_raw = pandas.read_csv("C:\\Users\\Maria\\Dropbox\\Sea Snails\\raw data\\vessels_labels_train.csv", index_col = 'vessel_id')
-
-port_visits_train_raw.head()
-
-
-vessels_labels_train_raw.apply(g)
-
-f = vessels_labels_train_raw.set_index('vessel_id')
-
-f['meeting_id1_row'] = meetings_train_raw.reset_index().groupby('ves_id1').index.aggregate(lambda x: tuple(x)).to_frame()
-f['meeting_id2_row'] = meetings_train_raw.reset_index().groupby('ves_id2').index.aggregate(lambda x: tuple(x))
-f['port_visits_row'] = port_visits_train_raw.reset_index().groupby('ves_id').index.aggregate(lambda x: tuple(x))
-f.reset_index().count()
+from os.path import expanduser
+try:
+   import cPickle as pickle
+except:
+   import pickle
 
 
-# In[131]:
+home = expanduser("~")
 
-f.head()
+# load raw data
+# print('loading data from CSV...')
+# meetings_train_raw = pandas.read_csv(home+"\\Dropbox\\Sea Snails\\raw data\\meetings_train.csv", nrows = None)
+# port_visits_train_raw = pandas.read_csv(home+"\\Dropbox\\Sea Snails\\raw data\\port_visits_train.csv", nrows = None)
+# vessels_labels_train_raw = pandas.read_csv(home+"\\Dropbox\\Sea Snails\\raw data\\vessels_labels_train.csv", index_col = 'vessel_id')
 
+# print('saving to pickle file...')
+# pickle.dump( meetings_train_raw, open( home+"\\Dropbox\\Sea Snails\\raw data\\meetings_train.p", "wb" ) )
+# pickle.dump( port_visits_train_raw, open( home+"\\Dropbox\\Sea Snails\\raw data\\port_visits_train_raw.p", "wb" ) )
+# pickle.dump( vessels_labels_train_raw, open( home+"\\Dropbox\\Sea Snails\\raw data\\vessels_labels_train_raw.p", "wb" ) )
 
-# In[109]:
+print('loading data from pickle file...')
+meetings_train_raw = pickle.load( open( home+"\\Dropbox\\Sea Snails\\raw data\\meetings_train.p", "rb" ) )
+port_visits_train_raw = pickle.load( open( home+"\\Dropbox\\Sea Snails\\raw data\\port_visits_train_raw.p", "rb" ) )
+vessels_labels_train_raw = pickle.load( open( home+"\\Dropbox\\Sea Snails\\raw data\\vessels_labels_train_raw.p", "rb" ) )
 
-g.first()
+#create sample data
+# meetings_train_raw = meetings_train_raw[0:1000]
+# port_visits_train_raw = port_visits_train_raw[0:1000]
 
+# create list of index of vessles to records in 'meetings' and in 'port visits' by row number
+print('creating vessle data...')
+data_per_vessel = vessels_labels_train_raw.copy()
+print('creating vessle data...meetings 1...')
 
-# In[ ]:
+data_per_vessel['meeting_id1_row'] = meetings_train_raw.reset_index().groupby('ves_id1').index.aggregate(lambda x: tuple(x))
+print('creating vessle data...meetings 2...')
+data_per_vessel['meeting_id2_row'] = meetings_train_raw.reset_index().groupby('ves_id2').index.aggregate(lambda x: tuple(x))
+print('creating vessle data...port visit...')
+data_per_vessel['port_visits_row'] = port_visits_train_raw.reset_index().groupby('ves_id').index.aggregate(lambda x: tuple(x))
 
+# save vessle data to pickle
+pickle.dump( data_per_vessel, open( home+"\\Dropbox\\Sea Snails\\preproc data\\data_per_vessel.p", "wb" ) )
 
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[12]:
+# add features
+print('adding features...')
+data_per_vessel['num_of_meetings'] = len(data_per_vessel['meeting_id1_row'])+len(data_per_vessel['meeting_id2_row'])
 
 meetings_train_sample = meetings_train_raw[0:100]
 port_visits_train_sample = port_visits_train_raw[0:100]
 
 
-# In[63]:
-
 c = pandas.Series.unique(meetings_train_sample['ves_id1'])
 
 
-# In[64]:
 
 labels = vessels_labels_train_raw.loc[c]
 
-
-# In[85]:
 
 print(labels[0:5])
 
@@ -81,47 +78,8 @@ meeting_arr.head()
 labels.head(1)
 
 
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-final_table
-
-
-# In[38]:
 
 print(labels)
-
-
-# In[ ]:
-
-
-
-
-# In[68]:
 
 len(labels)
 
@@ -139,11 +97,6 @@ type(meeting_arr)
 # In[25]:
 
 result = c.join(vessels_labels_train_raw, on='ves_id1')
-
-
-# In[ ]:
-
-
 
 
 # In[14]:
